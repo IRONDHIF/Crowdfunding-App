837b24c62182a5041e99aafaa0f5bc00300b4a22
IRONDHIF <abdan.nadhif@gmail.com>
2025-05-18 16:17:55 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
-/** Deskripsi:
-Aplikasi ini memungkinkan pengguna untuk melihat dan mencatat kontribusi
-terhadap proyek crowdfunding. Data utama yang digunakan adalah daftar proyek,
-target dana, dan jumlah kontribusi. Pengguna aplikasi adalah individu yang ingin
-berpartisipasi dalam proyek crowdfunding atau pemilik proyek yang mencari
-pendanaan.
-Spesifikasi:
-a. b. c. d. e. Pengguna dapat menambahkan, mengubah, dan menghapus proyek
-crowdfunding yang mereka buat.
-Sistem mencatat jumlah dana yang terkumpul dan jumlah donatur.
-Pengguna dapat mencari proyek berdasarkan nama atau kategori menggunakan
-Sequential dan Binary Search.
-Pengguna dapat mengurutkan proyek berdasarkan total dana terkumpul atau
-jumlah donatur menggunakan Selection dan Insertion Sort.
-Sistem menampilkan daftar proyek yang sudah mencapai target pendanaan.
-**/
-
 package main
 
 import "fmt"
